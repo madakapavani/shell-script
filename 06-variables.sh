3cc19ff4159ab00e16a9bed7cd52d50507424bde
pavani <pvr9602@gmail.com>
2025-01-04 19:23:03 +0530

    conversation by args

---
+++ 06-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you?"
+echo "$PERSON2:: Iam fine. What about you?"
+echo "$PERSON1:: Good. What are you doing now?"
+echo "$PERSON2:: Iam studying Degree, You?"
+echo "$PERSON1:: Iam doing Btech"
