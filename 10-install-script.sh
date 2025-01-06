9907ea7d471613aba4e7ec6bb4acd4ab35b7bc0b
Pavani <pvr9602@gmail.com>
2025-01-06 18:23:35 +0530

    mysql installed

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -13,7 +13,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
    then
         dnf install mysql -y
-        if [$? -ne 0]
+        if [ $? -ne 0 ]
         then
             echo "installing MYSQL...FAILURE"
          exit 1
