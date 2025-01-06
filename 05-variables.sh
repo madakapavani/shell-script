75ee8e9b67c95162302c3e788ee16b4f4d2336f2
Pavani <pvr9602@gmail.com>
2025-01-06 14:20:36 +0530

    Username and Password

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bass
 
 echo "Please enter your name"
-USERNAME
+read USERNAME
  
 echo "username entered"
 
 echo "please enter your password"
-PASSWORD
+read PASSWORD
 
 echo "password entered"
