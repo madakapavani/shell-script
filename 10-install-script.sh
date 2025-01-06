249e4ca0e9e76bae18853947235c00a6802190c5
Pavani <pvr9602@gmail.com>
2025-01-06 18:15:32 +0530

    mysql installed

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
    then
         echo "ERROR: you must have sudo access to execute this script"
    exit 1
@@ -9,7 +9,7 @@ fi
 
 dnf list installed mysql
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
    then
         dnf install mysql -y
         if [$? -ne 0]
